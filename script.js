ba5cdf381aa9a3c7d7f4043d5cd457da8434aedd
Maks Nazarets <48256242+MaksNazarets@users.noreply.github.com>
2023-11-11 20:49:20 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.menu-burger')
+// const menu = document.querySelector('#menu')
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('open')
+})
