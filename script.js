b49cab6388032c95d5ecb59d5eb9069ed47a61f1
unknown <nazarets.maxim@gmail.com>
2025-01-31 18:34:43 +0200

    optimize burger-menu styles and behavior

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const burger = document.querySelector(".menu-burger");
 const header = document.querySelector("header");
+const navLinks = document.querySelectorAll("#menu a");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("open");
 });
+
+navLinks.forEach((l) =>
+  l.addEventListener("click", () => burger.classList.remove("open"))
+);
 
 window.onscroll = () => {
   if (window.scrollY > 40) header.classList.add("sticky");
