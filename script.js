1b26ecb8515b642cd017157146395efd09e0bf4d
unknown <nazarets.maxim@gmail.com>
2025-01-31 17:51:25 +0200

    add favicon, make header sticky

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const burger = document.querySelector('.menu-burger')
+const burger = document.querySelector(".menu-burger");
+const header = document.querySelector("header");
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('open')
-})
+burger.addEventListener("click", () => {
+  burger.classList.toggle("open");
+});
+
+window.onscroll = () => {
+  if (window.scrollY > 40) header.classList.add("sticky");
+  else header.classList.remove("sticky");
+};
